ed0284401dc6e3e16cab8486aa1a0390a0edbae5
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-04-01 04:33:36 +0400

    Added Serializers

---
+++ Project/patient/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Patient
+
+
+# class PatientSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=100)
+#     phone = serializers.CharField(max_length=15, unique=True)
+#     email = serializers.EmailField(unique=True)
+#     password = serializers.CharField(max_length=100)
+#     dateOfBirth = serializers.DateField()
+#     gender = serializers.CharField(max_length=6)
+#     emiratesID = serializers.IntegerField(max=999999999999999)
+#     industry = serializers.CharField(max_length=100)
+#     locationLat = serializers.FloatField()
+#     locationLng = serializers.FloatField()
+#     determination = serializers.BooleanField(default=False)
+#     dosesTaken = serializers.IntegerField(default=0, max=2)
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ('email', 'name', 'gender')
+
+    # def create(self, validated_data):
+    #     return Patient.objects.create(validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.email = validated_data.get('email', instance.email)
