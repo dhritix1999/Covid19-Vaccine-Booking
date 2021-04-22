2bcf07dad8f659036750636220d0ccf3c1d9d53e
Azadan <azadanhb99@gmail.com>
2021-04-22 10:32:34 +0400

    Added Patient-MedicalIssue Relationship

--- Project/patient/models.py
+++ Project/patient/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class MedicalIssue(models.Model):
+    name = models.CharField(max_length=100)
+    vaccineEligibility = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
@@ -17,12 +23,7 @@ class Patient(models.Model):
     determination = models.BooleanField(default=False)
     dosesTaken = models.IntegerField(default=0)
 
+    patientMedicalIssues = models.ManyToManyField(MedicalIssue)
+
     def __str__(self):
         return self.email
-
-class MedicalIssue(models.Model):
-    name = models.CharField(max_length=100)
-    vaccineEligibility = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
--- Project/patient/serializers.py
+++ Project/patient/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Patient, MedicalIssue
 
+class MedicalIssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalIssue
+        fields = '__all__'
+
 
 class PatientSerializer(serializers.ModelSerializer):
+    patientMedicalIssues = serializers.PrimaryKeyRelatedField(many=True, allow_null=True, queryset=MedicalIssue.objects.all())
+
     class Meta:
         model = Patient
         fields = '__all__'
@@ -12,11 +19,4 @@ class PatientSerializer(serializers.ModelSerializer):
     #
     # def update(self, instance, validated_data):
     #     instance.email = validated_data.get('email', instance.email)
-
-
-class MedicalIssueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MedicalIssue
-        fields = '__all__'
-
 
