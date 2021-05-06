8e597c134c2a020ffb39792f202a6b01673dad89
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-06 23:35:46 +0400

    ClientApp: Updated registration

--- Project/clientApp/static/templates/js/profile/register.js
+++ Project/clientApp/static/templates/js/profile/register.js
@@ -1,7 +1,56 @@
 $(document).ready(function () {
 
+    // multi select list drop down
+    $('.selectpicker').selectpicker();
 
+    //get industries
+    $("#industry").empty()
+    loadIndustries('/api/industry/')
+
+    //get medical problems
+     $("#patientMedicalIssues").empty()
+    loadMedicalIssues('/api/medical-issue/')
+
+    //register form
     const patientForm = $("#form");
 
-        formSubmit(form, false, ENTITY_NAME+ ' created successfully', convertFormToJSONString, '/')
+    //form submission
+    formSubmit(patientForm, false, 'Patient registered successfully', convertFormToJSONString, '/')
 });
+
+function loadIndustries(url) {
+    $.ajax({
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        url: url,
+        success: function (data) {
+            console.log(data)
+            for (var i = 0; i < data.length; i++) {
+                var value = data[i].name;
+                $("#industry").append("<option value='" + value + "'>" + value + "</option>");
+            }
+        },
+        error: function (data) {
+            console.log(data)
+        },
+    });
+}
+
+function loadMedicalIssues(url){
+       $.ajax({
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        url: url,
+        success: function (data) {
+            console.log(data)
+            for (var i = 0; i < data.length; i++) {
+                var value = data[i].name;
+                $("#patientMedicalIssues").append("<option value='" + value + "'>" + value + "</option>");
+            }
+             $('.selectpicker').selectpicker('refresh');
+        },
+        error: function (data) {
+            console.log(data)
+        },
+    });
+}
