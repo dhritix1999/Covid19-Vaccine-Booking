484ec7d6e7bc30f660294c9fda7dc1c072103d89
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-07 17:28:38 +0400

    ClientApp: Fixed urls

--- Project/clientApp/urls.py
+++ Project/clientApp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .viewController.authentication import authenticationViews
+from .viewController import authenticationViews, patientViews
 
 urlpatterns = [
 
     path('', authenticationViews.index),
     path('register', authenticationViews.patient_register),
     path('admins', authenticationViews.admin_homepage),
-    path('patient', authenticationViews.patient_homepage)
+    path('patient', authenticationViews.patient_homepage),
+
+    path('patient/booking/list', patientViews.allowed_bookings),
+
+
 
 ]
