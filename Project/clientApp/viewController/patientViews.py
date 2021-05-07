72be6628f3c198fcd554ac38b21b02d15063307f
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-07 17:02:39 +0400

    ClientApp: Updated views

---
+++ Project/clientApp/viewController/patientViews.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+# booking table
+def allowed_bookings(request):
+    return render(request, 'patient/bookings/bookings-table.html')
+
