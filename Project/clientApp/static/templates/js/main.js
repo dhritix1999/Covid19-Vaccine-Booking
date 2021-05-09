e545a3c8f6ad8e5b7c8fee01ba259667ce9dfc63
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-09 06:56:18 +0400

    ClientApp: Updated base files

--- Project/clientApp/static/templates/js/main.js
+++ Project/clientApp/static/templates/js/main.js
@@ -152,3 +152,24 @@ function getDataFromUrl(url) {
         },
     });
 }
+
+
+
+//Get cookie routine by Shelley Powers
+function get_cookie(Name) {
+  var search = Name + "="
+  var returnvalue = "";
+  if (document.cookie.length > 0) {
+    offset = document.cookie.indexOf(search)
+    // if cookie exists
+    if (offset != -1) {
+      offset += search.length
+      // set index of beginning of value
+      end = document.cookie.indexOf(";", offset);
+      // set index of end of cookie value
+      if (end == -1) end = document.cookie.length;
+      returnvalue=unescape(document.cookie.substring(offset, end))
+      }
+   }
+  return returnvalue;
+}
