71f5d935464fad0b771429d02b1f30d1a59b68db
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-09 06:55:58 +0400

    ClientApp: Updated urls and views

--- Project/clientApp/urls.py
+++ Project/clientApp/urls.py
@@ -3,14 +3,31 @@ from .viewController import authenticationViews, patientViews
 
 urlpatterns = [
 
+    # authentication
     path('', authenticationViews.index),
     path('register', authenticationViews.patient_register),
+
+    # homepage
     path('admins', authenticationViews.admin_homepage),
     path('patient', authenticationViews.patient_homepage),
 
-    path('patient/booking/list', patientViews.allowed_bookings),
+    # patient profile
+    path('patient/profile/view', patientViews.view_profile),
+    path('patient/profile/update', patientViews.update_profile),
 
+    # patient booking
+    path('patient/booking/list', patientViews.allowed_bookings),
+    path('patient/booking/start', patientViews.start_booking),
+    path('patient/booking/history', patientViews.booking_history),
+
+    # industry
     path('admins/industry', authenticationViews.admin_industry),
+
+    # priority
+
+
+    # vaccine centers
+
 
 
 
--- Project/clientApp/viewController/patientViews.py
+++ Project/clientApp/viewController/patientViews.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 # booking table
 def allowed_bookings(request):
     return render(request, 'patient/bookings/bookings-table.html')
 
+
+def start_booking(request):
+    return None
+
+
+def booking_history(request):
+    return None
+
+
+def view_profile(request):
+    return render(request, 'patient/profile/view-profile.html')
+
+
+def update_profile(request):
+    return render(request, 'patient/profile/edit-profile.html')
