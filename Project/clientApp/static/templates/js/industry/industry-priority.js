cc1f52abb767e603831cb4319350ed96baec228b
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-11 22:48:11 +0400

    ClientApp: View and delete industry completed js

---
+++ Project/clientApp/static/templates/js/industry/industry-priority.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    get_industries('/api/industry/')
+
+});
+
+function get_industries(url){
+    $.ajax({
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        url: url,
+        success: function (data) {
+            console.log(data)
+
+            for (var i = 0; i < data.length; i++) {
+                $('#dataTable').DataTable().row.add( [
+                    data[i].id,
+                    data[i].name,
+                    data[i].priority,
+                        '<a th:href="@{/users/edit/{login}(login=${user.login})}" class="edit"><i\n' +
+                        '                                class="material-icons" data-toggle="tooltip"\n' +
+                        '                                title="Edit">&#xE254;</i></a>'+
+                        '    <a  onclick="deleteEntity(&quot/api/industry/'+data[i].id+'&quot)" class="delete" style="cursor: pointer"><i\n' +
+                        '                                class="material-icons" data-toggle="tooltip"\n' +
+                        '                                title="Delete">&#xe888;</i></a>'
+                ] ).draw( false );
+            }
+        },
+        error: function (a, jqXHR, exception) { }
+    });
+}
