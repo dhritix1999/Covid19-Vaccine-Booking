cd4d20c3333bebd8984698799e9ab2bf9bf2b1cc
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-15 01:36:27 +0400

    ClientApp: updated authentication views

--- Project/clientApp/viewController/authenticationViews.py
+++ Project/clientApp/viewController/authenticationViews.py
@@ -21,18 +21,4 @@ def admin_homepage(request):
     return render(request, 'admin/homepage.html')
 
 
-def industry_list(request):
-    return render(request, 'admin/industry/industry-priority-table.html')
 
-
-def add_industry(request):
-    context = {"edit_industry": False}
-    return render(request, 'admin/industry/add-edit-industry.html', context)
-
-
-def edit_industry(request, pk):
-    context = {
-        "edit_industry": True,
-        "industryID": pk
-    }
-    return render(request, 'admin/industry/add-edit-industry.html', context)
